5d9808ba076e24d5bf037e261072c079bc39688d
Laíza  Pimentel <laizahpimentel@gmail.com>
2023-09-01 12:03:55 -0300

    variavel

---
+++ src/br/code/letscode/java/App.java
@@ -0,0 +1,24 @@
+package br.code.letscode.java;
+
+public class App {
+    // variável
+    public static void main(String[] args) throws Exception {
+
+        // String nome = "Laiza";
+        // System.out.println("Olá," + nome);
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        // int divisao = a / b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}

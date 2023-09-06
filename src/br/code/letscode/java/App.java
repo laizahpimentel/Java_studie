bda8a150f78d7c5866d035c5a8fb525bbddaf9d7
Laíza  Pimentel <laizahpimentel@gmail.com>
2023-09-05 21:31:39 -0300

    estruturas condicionais

--- src/br/code/letscode/java/App.java
+++ src/br/code/letscode/java/App.java
@@ -1,34 +1,91 @@
 package br.code.letscode.java;
 
 public class App {
-    // Operadores
+    // EstruturasCondicionais
     public static void main(String[] args) {
 
-        boolean fimDeSemana = true;
-        boolean fazendoSol = false;
-        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        int nota = 50;
+        String graduacao;
 
-        // tabela verdade
+        if (nota >= 80) {
+            graduacao = "A";
+            // System.out.println("graduaçao A");
+        } else if (nota < 80 && nota >= 70) {
+            graduacao = "B";
+            // System.out.println("Graduaçao B");
+        }
+        if (nota < 70 && nota >= 60) {
+            graduacao = "C";
+            // System.out.println("Graduaçao C");
+        }
+        if (nota < 60 && nota >= 0) {
+            graduacao = "D";
+            // System.out.println("Graduaçao D");
+        } else {
+            graduacao = null;
+            // System.out.println("Nota invalida");
+        }
 
-        // Operador && (AND)
-        // true && true = true
-        // true &&false = false
-        // false && true = false
-        // false && false = false
+        // Opçao de SWITCH com else/if
 
-        // Operador || (OR)
-        // true || true = true
-        // true || false = true
-        // false || true = true
-        // false || false = false
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado");
+                break; // interrompidp
+            case "C":
+            case "D":
+                System.out.println("Nao aprovado ");
+                break; //
+            default:
+                System.out.println("Graduacao Invalida");
+        }
 
-        System.out.println(vamosAPraia);
+        // int nota = 70;
 
-        String mensagem = fimDeSemana ? "é fim de semana " : "Nao é fim de semana";
-        System.out.println(mensagem);
+        // se nota maior ou igual 70, entao aprovado
+        // if-else
+
+        // if (nota >= 70) {
+        // System.out.println("Aluno aprovado!");
+        // }
+
+        // if (nota <= 70) {
+        // System.out.println("Aluno Reprovador!");
+        // }
     }
 }
 
+// package br.code.letscode.java;
+
+// public class App {
+// Operadores
+// public static void main(String[] args) {
+
+// boolean fimDeSemana = true;
+// boolean fazendoSol = false;
+// boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+// tabela verdade
+
+// Operador && (AND)
+// true && true = true
+// true &&false = false
+// false && true = false
+// false && false = false
+
+// Operador || (OR)
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+// System.out.println(vamosAPraia);
+
+// String mensagem = fimDeSemana ? "é fim de semana " : "Nao é fim de semana";
+// System.out.println(mensagem);
+// }
+// }
 // package br.code.letscode.java;
 
 // public class App {
---
+++ src/br/code/letscode/java/Strings.java
@@ -0,0 +1,5 @@
+package br.code.letscode.java;
+
+public class Strings {
+
+}
